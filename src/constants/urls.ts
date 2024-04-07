c876d51ed70c813461a31c4c54bc8d5e3dbc6b98
SoloProgrammer <prathamshinde987@gmail.com>
2024-04-08 02:15:10 +0530

    Created seperare constant urls file

---
+++ src/constants/urls.ts
@@ -0,0 +1,5 @@
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+
+export const LOGIN_URI = `${SERVER_URL}/api/auth/callback/google`;
+
+export const SESSION_URI = `${SERVER_URL}/api/auth/session`;
